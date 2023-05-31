1685795f5afaf5f85869f56989c460e631385d83
maruf9270 <maruf9270@gmail.com>
2023-05-31 11:17:25 -0700

    [done] setup express, mngoose, cors, parser

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,14 @@
 import express, { Application, Request,Response } from "express"
+import cors from 'cors'
 export const app:Application = express();
 export const port = process.env.PORT || 5000;
+
+// Using cors
+app.use(cors())
+
+// Using body parser
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 // For testing purpose api route
 app.get('/',(req:Request, res:Response )=>{
