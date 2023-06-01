e0cd3d9b49df07a6ee1af18318cfa8862ceba618
maruf9270 <maruf9270@gmail.com>
2023-06-01 13:59:55 -0700

    [done] created user model and user services to create new user

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import userRoute from './app/modules/users/user.route'
 export const app: Application = express()
 export const port = process.env.PORT || 5000
 
@@ -11,6 +12,9 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // For testing purpose api route
-app.get('/', (req: Request, res: Response) => {
+app.get('/', async (req: Request, res: Response) => {
   res.send('Server is working')
 })
+
+// Application routes
+app.use('/api/v1/users/', userRoute)
---
+++ src/app/modules/users/user.service.ts
@@ -0,0 +1,25 @@
+import config from '../../../config'
+import { Iuser } from './user.interface'
+import { User } from './user.model'
+import { generateUserId } from './user.utils'
+
+const createUser = async (user: Iuser): Promise<Iuser | null> => {
+  // Need to use a auto incremental id and a default password
+
+  const id = await generateUserId()
+  user.id = id
+  if (!user.password) {
+    user.password = config.default_user_pass as string
+  }
+  if (!createUser) {
+    throw new Error('failed to create user')
+  }
+
+  const createdUser = await User.create(user)
+
+  return createdUser
+}
+
+export default {
+  createUser,
+}
