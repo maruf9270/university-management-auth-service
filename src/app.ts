24f0b046cd60451178e0638fe8475a921e48079b
maruf9270 <maruf9270@gmail.com>
2023-06-01 10:46:14 -0700

    Testing husky

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,16 @@
-import express, { Application, Request,Response } from "express"
+import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
-export const app:Application = express();
-export const port = process.env.PORT || 5000;
+export const app: Application = express()
+export const port = process.env.PORT || 5000
 
 // Using cors
 app.use(cors())
 
 // Using body parser
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
 // For testing purpose api route
-app.get('/',(req:Request, res:Response )=>{
-    res.send("Server is working")
+app.get('/', (req: Request, res: Response) => {
+  res.send('Server is working')
 })
-
